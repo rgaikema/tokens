7f3ca335df5b07f2841c17a3cd72f449d66a2b79
Remco Gaikema <mail@remcogaikema.com>
2022-09-12 10:55:57 +0200

    created test build

---
+++ build.js
@@ -0,0 +1,15 @@
+const StyleDictionary = require('style-dictionary').extend({
+    source: ['tokens.json'],
+    platforms: {
+      scss: {
+        transformGroup: 'scss',
+        buildPath: 'build/',
+        files: [{
+          destination: 'variables.scss',
+          format: 'scss/variables'
+        }]
+      }
+    }
+  });
+  
+  StyleDictionary.buildAllPlatforms();
